51c838dacc235ca03c36e0cd99529f8841588911
flywire <flywire0@gmail.com>
2020-12-13 15:04:09 +1100

    Test configs with clearer differences
    and removed all but a couple of `config params` defined the same as defaults

--- mdx_wikilink_plus/test.py
+++ mdx_wikilink_plus/test.py
@@ -11,12 +11,12 @@ def build_url(urlo, base, end, url_whitespace, url_case):
     return "https://dummy"
 
 meta_text = """
-wiki_base_url: /static/
-wiki_end_url: 
+wiki_base_url: /local
 wiki_url_whitespace: _
 wiki_url_case: lowercase
 wiki_label_case: capitalize
-wiki_html_class: wikilink
+wiki_html_class: wiki-lnk
+wiki_image_class: wiki-img
 """.strip()
 
 text = """
@@ -59,9 +59,7 @@ md_configs1 = {
                 'mdx_wikilink_plus': {
                     'base_url': '/static',
                     'end_url': '.html',
-                    'url_whitespace': '-',
                     'url_case': 'lowercase',
-                    'label_case': 'titlecase',
                     'html_class': 'a-custom-class',
                 },
              }
@@ -74,7 +72,7 @@ md_configs2 = {
                     'url_whitespace': '-',
                     'url_case': 'uppercase',
                     'label_case': 'titlecase',
-                    'html_class': 'a-custom-class',
+                    'image_class': 'wikilink',
                     'build_url': build_url,
                 },
              }
@@ -128,27 +126,28 @@ class TestMethods(unittest.TestCase):
         """.strip()
         md2 = markdown.Markdown(extensions=[WikiLinkPlusExtension(md_configs1['mdx_wikilink_plus'])]) 
         html = md2.convert(text)
+        # ~ print(html)
         self.assertEqual(html, output)
 
     def test_with_config2(self):
         output = """
-<p><a class="a-custom-class" href="https://dummy">Wikilink</a>    <code>[[wikilink]]</code></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">File Name</a></p>
-<p><a class="a-custom-class" href="https://dummy">www.example.com</a></p>
-<p><a class="a-custom-class" href="https://dummy">www.example.com</a></p>
-<p><a class="a-custom-class" href="https://dummy">Example Tutorial</a></p>
-<p><a class="a-custom-class" href="https://dummy">Example Tutorial</a></p>
-<p><img class="wikilink-image" src="https://dummy" /></p>
-<p><img class="wikilink-image" src="https://dummy" /></p>
-<p><img class="wikilink-image" src="https://dummy" /></p>
-<p><img class="wikilink-image" src="https://dummy" /></p>
-<p><img class="wikilink-image" src="https://dummy" /></p>
-<p><img alt="better example" class="wikilink-image" src="https://dummy" /></p>
+<p><a class="wikilink" href="https://dummy">Wikilink</a>    <code>[[wikilink]]</code></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">File Name</a></p>
+<p><a class="wikilink" href="https://dummy">www.example.com</a></p>
+<p><a class="wikilink" href="https://dummy">www.example.com</a></p>
+<p><a class="wikilink" href="https://dummy">Example Tutorial</a></p>
+<p><a class="wikilink" href="https://dummy">Example Tutorial</a></p>
+<p><img class="wikilink" src="https://dummy" /></p>
+<p><img class="wikilink" src="https://dummy" /></p>
+<p><img class="wikilink" src="https://dummy" /></p>
+<p><img class="wikilink" src="https://dummy" /></p>
+<p><img class="wikilink" src="https://dummy" /></p>
+<p><img alt="better example" class="wikilink" src="https://dummy" /></p>
         """.strip()
         md2 = markdown.Markdown(extensions=[WikiLinkPlusExtension(md_configs2['mdx_wikilink_plus'])]) 
         html = md2.convert(text)
@@ -158,23 +157,23 @@ class TestMethods(unittest.TestCase):
 
     def test_with_meta(self):
         output = """
-<p><a class="wikilink" href="/static/wikilink">Wikilink</a>    <code>[[wikilink]]</code></p>
-<p><a class="wikilink" href="/static/path/to/file_name">File name</a></p>
-<p><a class="wikilink" href="/static/path/to/file_name">File name</a></p>
-<p><a class="wikilink" href="/static/path/to/file-name">File name</a></p>
-<p><a class="wikilink" href="/static/path/to/file_name/?a=b&amp;b=c">File name</a></p>
-<p><a class="wikilink" href="/static/path/to/file_name.html">File name</a></p>
-<p><a class="wikilink" href="/static/path/to/file_name.html?a=b&amp;b=c">File name</a></p>
-<p><a class="wikilink" href="https://www.example.com/">www.example.com</a></p>
-<p><a class="wikilink" href="https://www.example.com/?a=b&amp;b=c">www.example.com</a></p>
-<p><a class="wikilink" href="https://www.example.com/example-tutorial">Example tutorial</a></p>
-<p><a class="wikilink" href="https://www.example.com/example-tutorial">Example Tutorial</a></p>
-<p><img class="wikilink-image" src="/static/wikilink.png" /></p>
-<p><img class="wikilink-image" src="/static/path/to/file_name.jpg?a=b&amp;b=c" /></p>
-<p><img class="wikilink-image" src="https://example.jpeg?a=b&amp;b=c" /></p>
-<p><img class="wikilink-image" src="https://www.example.com/example-tutorial.jpeg" /></p>
-<p><img class="wikilink-image" src="https://example.com/example-tutorial.gif" /></p>
-<p><img alt="better example" class="wikilink-image" src="/static/example_tutorial.jpg" /></p>
+<p><a class="wiki-lnk" href="/local/wikilink">Wikilink</a>    <code>[[wikilink]]</code></p>
+<p><a class="wiki-lnk" href="/local/path/to/file_name">File name</a></p>
+<p><a class="wiki-lnk" href="/local/path/to/file_name">File name</a></p>
+<p><a class="wiki-lnk" href="/local/path/to/file-name">File name</a></p>
+<p><a class="wiki-lnk" href="/local/path/to/file_name/?a=b&amp;b=c">File name</a></p>
+<p><a class="wiki-lnk" href="/local/path/to/file_name.html">File name</a></p>
+<p><a class="wiki-lnk" href="/local/path/to/file_name.html?a=b&amp;b=c">File name</a></p>
+<p><a class="wiki-lnk" href="https://www.example.com/">www.example.com</a></p>
+<p><a class="wiki-lnk" href="https://www.example.com/?a=b&amp;b=c">www.example.com</a></p>
+<p><a class="wiki-lnk" href="https://www.example.com/example-tutorial">Example tutorial</a></p>
+<p><a class="wiki-lnk" href="https://www.example.com/example-tutorial">Example Tutorial</a></p>
+<p><img class="wiki-img" src="/local/wikilink.png" /></p>
+<p><img class="wiki-img" src="/local/path/to/file_name.jpg?a=b&amp;b=c" /></p>
+<p><img class="wiki-img" src="https://example.jpeg?a=b&amp;b=c" /></p>
+<p><img class="wiki-img" src="https://www.example.com/example-tutorial.jpeg" /></p>
+<p><img class="wiki-img" src="https://example.com/example-tutorial.gif" /></p>
+<p><img alt="better example" class="wiki-img" src="/local/example_tutorial.jpg" /></p>
         """.strip()
         md2 = markdown.Markdown(extensions=[WikiLinkPlusExtension(), MetaExtension()]) 
         html = md2.convert(meta_text+"\n\n"+text)
